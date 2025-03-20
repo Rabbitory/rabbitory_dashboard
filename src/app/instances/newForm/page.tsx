a8edae0369c4fdb04b103bc456f9eb0c42e48f3b
Mason <31372737+Ovaculos@users.noreply.github.com>
2025-03-20 15:02:07 -0500

    Added loading for regions

--- src/app/instances/newForm/page.tsx
+++ src/app/instances/newForm/page.tsx
@@ -11,6 +11,7 @@ const instanceTypes = ["t2.micro", "t2.small", "t2.medium"];
 
 export default function NewFormPage() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const [instanceName, setInstanceName] = useState("");
   const [availableRegions, setAvailableRegions] = useState([]);
   const [instantiating, setInstantiating] = useState(false);
@@ -18,8 +19,10 @@ export default function NewFormPage() {
   useEffect(() => {
     const fetchRegions = async () => {
       try {
+        setLoading(true);
         const { data } = await axios.get("/api/regions");
         setAvailableRegions(data.regions);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching regions:", error);
       }
@@ -50,47 +53,52 @@ export default function NewFormPage() {
   };
   return (
     <>
-      <Form
-        action={(formData) => {
-          setInstantiating(true);
-          handleSubmit(formData);
-        }}
-      >
-        <fieldset disabled={instantiating}>
-          <label htmlFor="instanceName">Instance Name:</label>
-          <input
-            id="instanceName"
-            name="instanceName"
-            type="text"
-            value={instanceName}
-            onChange={(e) => setInstanceName(e.target.value)}
-          />
-          <button type="button" onClick={handleGenerate}>
-            Generate Instance Name
-          </button>
-          <label htmlFor="region">Region: </label>
-          <select id="region" name="region" disabled={instantiating}>
-            {availableRegions.map((region) => (
-              <option key={region} value={region}>
-                {region}
-              </option>
-            ))}
-          </select>
-          <label htmlFor="instanceType">Instance Hardware: </label>
-          <select id="instanceType" name="instanceType">
-            {instanceTypes.map((type) => (
-              <option key={type} value={type}>
-                {type}
-              </option>
-            ))}
-          </select>
-          <label htmlFor="username">Username: </label>
-          <input id="username" name="username" type="text" />
-          <label htmlFor="password">Password: </label>
-          <input id="password" name="password" type="password" />
-          <button type="submit">Submit</button>
-        </fieldset>
-      </Form>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <Form
+          action={(formData) => {
+            setInstantiating(true);
+            handleSubmit(formData);
+          }}
+        >
+          <fieldset disabled={instantiating}>
+            <label htmlFor="instanceName">Instance Name:</label>
+            <input
+              id="instanceName"
+              name="instanceName"
+              type="text"
+              value={instanceName}
+              onChange={(e) => setInstanceName(e.target.value)}
+            />
+            <button type="button" onClick={handleGenerate}>
+              Generate Instance Name
+            </button>
+            <label htmlFor="region">Region: </label>
+            <select id="region" name="region" disabled={instantiating}>
+              {availableRegions.map((region) => (
+                <option key={region} value={region}>
+                  {region}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="instanceType">Instance Hardware: </label>
+            <select id="instanceType" name="instanceType">
+              {instanceTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="username">Username: </label>
+            <input id="username" name="username" type="text" />
+            <label htmlFor="password">Password: </label>
+            <input id="password" name="password" type="password" />
+            <button type="submit">Submit</button>
+          </fieldset>
+        </Form>
+      )}
+
       {instantiating ? <div className="loading">Creating instance...</div> : ""}
     </>
   );
