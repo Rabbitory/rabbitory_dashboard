1f96d51fd920ee6b232d22508527ecb1040195d6
larencozart <larencozart@gmail.com>
2025-03-22 09:48:21 -0500

    Add params to grab instance name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,25 @@
 import '@/app/global.css';
 import Link from 'next/link';
 
-export default function RootLayout({children}: Readonly<{
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+  params: Promise<{ instanceName: string }>;
+}
+
+
+export default async function RootLayout({ children, params }: Readonly<RootLayoutProps>) {
+  const {instanceName} = await params;
+  console.log("Current Instance:", instanceName);
+
   return (
     <html lang="en">
       <body>
         <div className="top-header">
           <h1>Rabbitory</h1>
-          <p >Current Instance Name</p>
+          <p>Current Instance Name: {instanceName}</p>
           <Link href="/">Home</Link>
         </div>
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
       </body>
     </html>
   );
