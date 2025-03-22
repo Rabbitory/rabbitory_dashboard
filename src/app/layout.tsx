529c2082a57b654f42970d0aaeec80a6eaf6f9f0
larencozart <larencozart@gmail.com>
2025-03-22 09:26:40 -0500

    Setup base header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import '@/app/global.css';
+import Link from 'next/link';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+export default function RootLayout({children}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>
-        <header className="top-header">
+        <div className="top-header">
           <h1>Rabbitory</h1>
-          {/* <p>Current Instance</p> */}
-        </header>
+          <p >Current Instance Name</p>
+          <Link href="/">Home</Link>
+        </div>
         <main>
           {children}
         </main>
