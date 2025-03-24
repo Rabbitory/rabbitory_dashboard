361a9d9c0de1537d1eda1ada7e21e2744ea0d650
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-03-24 12:23:01 -0500

    mock delete function, redirect on cancel

--- src/app/instances/[name]/edit/delete/page.tsx
+++ src/app/instances/[name]/edit/delete/page.tsx
@@ -1,6 +1,13 @@
 "use client"
 
 import * as React from 'react';
+import {
+  useState,
+  useEffect,
+  ChangeEvent,
+  FormEvent
+} from 'react';
+import { useRouter } from "next/navigation";
 
 interface Params {
   name: string;
@@ -10,17 +17,37 @@ interface DeletePageParams {
 }
 
 export default function DeletePage({ params }: DeletePageParams) {
+  const [inputText, setInputText] = useState("");
+  const [validInput, setValidInput] = useState(false);
   const { name } = React.use(params);
+  const router = useRouter();
+
+  useEffect(() => {
+    const isValidInput = inputText === name;
+    setValidInput(isValidInput);
+  }, [inputText, name])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputText(value);
+    console.log(inputText);
+  }
+
+  const handleDelete = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('deleted');
+  }
+
   return (
     <>
       <h1>{name}</h1>
       <p><strong>By submitting the following form, this instance will be permanently deleted</strong></p>
-      <form action="" method="get">
+      <form action="" method="get" onSubmit={(e) => handleDelete(e)}>
         <label htmlFor='instance'>Type the instance name:</label>
-        <input type="text" name="instance" />
-        <input type="submit" value="Delete" />
-        <button type="button" >Cancel</button>
-      </form>
+        <input type="text" name="instance" onChange={(e) => handleChange(e)} />
+        <button type="submit" disabled={!validInput}>Delete</button>
+        <button type="button" onClick={() => router.push(`/instances/${name}`)}>Cancel</button>
+      </form >
     </>
   )
 }
