ce551c06449d693564c2d3a00349053ed721b49f
Jacqueline Amherst <jacqueline.amherst@icloud.com>
2025-03-23 19:43:58 -0500

    mock up delete page

--- src/app/instances/[name]/edit/delete/page.tsx
+++ src/app/instances/[name]/edit/delete/page.tsx
@@ -1,7 +1,26 @@
 "use client"
 
-export default function InstancePage() {
+import * as React from 'react';
+
+interface Params {
+  name: string;
+}
+interface DeletePageParams {
+  params: Promise<Params>;
+}
+
+export default function DeletePage({ params }: DeletePageParams) {
+  const { name } = React.use(params);
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>{name}</h1>
+      <p><strong>By submitting the following form, this instance will be permanently deleted</strong></p>
+      <form action="" method="get">
+        <label htmlFor='instance'>Type the instance name:</label>
+        <input type="text" name="instance" />
+        <input type="submit" value="Delete" />
+        <button type="button" >Cancel</button>
+      </form>
+    </>
   )
 }
