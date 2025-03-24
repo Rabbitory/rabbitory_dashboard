d1830d094c0b5c6e2571e3ef6a6a6a4c434a5b9d
larencozart <larencozart@gmail.com>
2025-03-24 16:51:12 -0500

    Merge nav bar

--- src/app/instances/[name]/layout.tsx
+++ src/app/instances/[name]/layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import React from 'react';
 import NavLayout from "@/app/components/NavLayout";
 import styles from "./InstanceLayout.module.css";
 
@@ -16,32 +18,15 @@ export default async function InstanceLayout({
   const { name } = await params;
   return (
     <>
-      <div className={styles.container}>
-        <NavLayout name={name} />
-        <section className={styles.section}>{children}</section>
-      </div>
-    </>
-  );
-}
-import Link from 'next/link';
-import React from 'react';
-
-interface InstanceLayoutProps {
-  children: React.ReactNode;
-  params: { name: string };
-}
-
-export default async function InstanceLayout({ children, params }: Readonly<InstanceLayoutProps>) {
-  const { name } = await params;
-  
-  return (
-    <>
       <div className="top-header">
         {/* <h1>Rabbitory</h1> */}
         <p>{name}</p>
         <Link href="/">All Instances</Link>
       </div>
-      {children}
+      <div className={styles.container}>
+        <NavLayout name={name} />
+        <section className={styles.section}>{children}</section>
+      </div>
     </>
   );
 }
