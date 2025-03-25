eeb2f25d953d968ec841b0dbeccfd053f6b95b17
larencozart <larencozart@gmail.com>
2025-03-25 11:59:18 -0500

    Fix link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
       </head>
       <body className="bg-gray-100 text-gray-900 font-plex">
         <div className="top-header flex justify-between items-center bg-orange-300 text-white p-4">
-          <h1 className="text-3xl font-semibold">Rabbitory</h1>
-          <Link href="/" className="text-lg text-white hover:underline">
-            All Instances
-          </Link>
+          <h1 className="text-3xl font-semibold">
+          <Link href="/" className="text-lg text-white hover:underline">Rabbitory</Link>
+          </h1>
+          
         </div>
         <main>{children}</main>
       </body>
