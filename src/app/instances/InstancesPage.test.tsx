893e42546c055514adc144564599d70f3b0aa058
larencozart <larencozart@gmail.com>
2025-03-27 13:34:02 -0500

    Fix InstancesPageTes for ci pipe

--- src/app/instances/InstancesPage.test.tsx
+++ src/app/instances/InstancesPage.test.tsx
@@ -2,7 +2,17 @@ import Home from "./page";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-it("renders layout", () => {
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(), // mock the `push` method if needed
+  }),
+}));
+
+
+it("renders layout with title and button", () => {
   render(<Home />);
+
   expect(screen.getByText("Instances")).toBeInTheDocument();
+  expect(screen.getByText("Create New Instance")).toBeInTheDocument();
 });
+
