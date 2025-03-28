b15b00465096be99da014e2990e656bfbeca53ee
Mason <31372737+Ovaculos@users.noreply.github.com>
2025-03-27 22:58:13 -0500

    Fixed storage size on hardware page

--- src/app/instances/[name]/hardware/page.tsx
+++ src/app/instances/[name]/hardware/page.tsx
@@ -1,10 +1,34 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { useInstanceContext } from "../InstanceContext";
 import Link from "next/link";
+import axios from "axios";
 
 export default function HardwarePage() {
   const { instance } = useInstanceContext();
+  const [volumeSize, setVolumeSize] = useState<number | "Loading...">(
+    "Loading...",
+  );
+
+  useEffect(() => {
+    async function fetchVolumeSize() {
+      if (!instance?.EBSVolumeId || !instance?.region) {
+        alert("Missing EBSVolumeId or region");
+        return;
+      }
+
+      const response = await axios.get(
+        `/api/instances/${instance?.name}/hardware/storage?volumeId=${instance?.EBSVolumeId}&region=${instance?.region}`,
+      );
+
+      if (response.data) {
+        setVolumeSize(response.data.size);
+      }
+    }
+
+    fetchVolumeSize();
+  }, [instance]);
 
   return (
     <>
@@ -14,7 +38,7 @@ export default function HardwarePage() {
         <Link href={`/instances/${instance?.name}/hardware/type/edit`}>
           Change instance type
         </Link>
-        <p>Current instance storage size: {instance?.EBSVolumeId}</p>
+        <p>Current instance storage size: {volumeSize}</p>
         <Link href={`/instances/${instance?.name}/hardware/storage/edit`}>
           Change storage size
         </Link>
